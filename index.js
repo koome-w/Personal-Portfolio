aa513a8e362a3993d8b3a26a9c9306b4da87d537
koome-w <koomekinya3@gmail.com>
2025-04-04 23:28:48 +0300

    git add . git commit -m

---
+++ index.js
@@ -0,0 +1,9 @@
+ // Optional smooth scroll animations or effects
+ document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      document.querySelector(link.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
